b82cf0828f1f233343553357c57cc6487de2f608
vmanamino <vmanamino@gmail.com>
2016-03-18 02:53:04 +0000

    animations with sound

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.ryu').mouseenter(function() {
-       $('.ryu-still').hide();
-       $('.ryu-ready').show();
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();   
     })
     .mouseleave(function() {
         $('.ryu-still').show();
         $('.ryu-ready').hide();
+    })
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate({
+            "left": "1020px"},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '606px');    
+            }
+        );
+    })
+    .mouseup(function() {
+        $('.ryu-ready').show();
+        $('.ryu-throwing').hide();
     });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
